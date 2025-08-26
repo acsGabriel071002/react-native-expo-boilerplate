dac5cc117d0313caa608b3ba8068aba256c5f1f4
Ramon Pratar <ramon.prata@dtidigital.com.br>
2025-08-26 14:17:29 -0300

    folder structure

---
+++ src/features/Products/views/styles/HighlightedProducts.styled.tsx
@@ -0,0 +1,7 @@
+import { BoxContent } from "@/src/shared/components";
+import styled from "@emotion/native";
+
+export const HighlightedProductsContainer = styled(BoxContent)({
+  padding: 16,
+  borderRadius: 8,
+});
